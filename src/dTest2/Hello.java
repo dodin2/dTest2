5e40273fa44fd0b395b98733d4248f09bc02ed76
Kim, Sojin <xozinee@gmail.com>
2023-10-16 20:24:43 +0900

    Main.java 수정 #3, Hello.java 수정 #3

--- src/dTest2/Hello.java
+++ src/dTest2/Hello.java
@@ -3,6 +3,10 @@ package dTest2;
 public class Hello {
     String msg = "hello";
 
+    public Hello(String msg) {
+        this.msg = msg;
+    }
+
     public void say() {
         System.out.println(msg);
     }
